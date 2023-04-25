a122b9a76b2567aa468dadec9701b3cf75940820
Sachin Prajapati <126360000+sachinparjapati011@users.noreply.github.com>
2023-04-25 21:41:25 +0530

    Delete covid_statics-master/Covid-Aggregate-API-Mongdb-BoilerPlate-main directory

--- covid_statics-master/Covid-Aggregate-API-Mongdb-BoilerPlate-main/src/createDatabase.js
+++
@@ -1,8 +0,0 @@
-const { connection } = require('./connector')
-const { data } = require('./data')
-const refreshAll = async () => {
-    await connection.deleteMany({})
-    await connection.insertMany(data)  
-}
-
-module.exports = refreshAll;
